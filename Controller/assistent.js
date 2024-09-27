f24b804aee4c44e10e04e5b617f8aab0370f1a61
Glitch (whatsapp-cloud-api-echo-bot) <none>
2024-09-27 18:22:34 +0000

    🎈👮 Checkpoint ./Controller/assistent.js:70709621/1771

--- Controller/assistent.js
+++ Controller/assistent.js
@@ -1,98 +1,15 @@
-const mongoose = require("mongoose")
-
-const historySchema = mongoose.Schema({
-  user: {
-    type: String,
-    required : true
-  },
-  message: {
-    type: String,
-    required: true
-  },
-  response: {
-    type: String,
-    required: false
-  },
-  date: {
-    type: Date,
-    required: true
-  },
-  dateFormat: {
-    type: String,
-    required: true
-  },
-  leido: {
-    type: String,
-    default: false
-  },
-  prompt_tokens: {
-    type:String,
-    required: false
-  },
-  completion_tokens: {
-    type:String,
-    required: false
-  },
-  tokens: {
-    type:Number,
-    required: false
-  },
-  prompt_tokens: {
-    type:String,
-    required: false
-  },
-  completion_tokens: {
-    type:String,
-    required: false
-  },
-  tipo:{
-    type: String,
-    require: false
-  },
-  image_id:{
-    type: mongoose.Types.ObjectId,
-    required: false
-  }
-})
-
-const historyModel = mongoose.model("histories", historySchema)
-module.exports   = historyModel
-
 const mongoose = require('mongoose');
 
-const SubSchema  = new mongoose.Schema({
-  user: {
+const messageSchema = new mongoose.Schema({
+  content: {
     type: String,
-    required: true
+    required: true,
   },
-  name:{
-    type: String,
-    required: false
+  createdAt: {
+    type: Date,
+    default: Date.now,
   },
-  active: {
-    type: Boolean,
-    required: false
-  },
-  plan: {
-    type: String,
-    required: false
-  },
-  cantidad: {
-    type: Number,
-    required: false
-  },
-  active_date: {
-    type: String,
-    required: false
-  },
-  suspension_date: {
-    type: String,
-    required: false
-  }
-})
+});
 
-
-const Subscription = mongoose.model('subscriptions', SubSchema)
-
-module.exports     = Subscription
+module.exports = mongoose.model('Message', messageSchema);
 
